c191822d991f8265e36faa55b88fe2c7ef033db3
Godwin Daniel <godwinolele10@gmail.com>
2021-04-09 21:07:16 +0100

    first commit on this project

---
+++ script/app.js
@@ -0,0 +1,5 @@
+const tl = gsap.timeline({defaults: {ease: "power1.out"} });
+
+tl.to ('.text', {y:"0%", duration: 1, stagger: 0.25 });
+tl.to('.slider', {y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to('.intro', { y: "-100%", duration: 1 }, "-=1" );
