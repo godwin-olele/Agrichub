89304350e2657d627a9b5feb207da387346a234f
godwin olele <godwinolele10@gmail.com>
2021-07-10 09:47:14 +0100

    changed the size of illustration

--- script/index.js
+++ script/index.js
@@ -1,3 +1,11 @@
+const signupBtn = document.querySelector("#signup-Btn");
+const introSelector = document.querySelector("#intro");
+const buttonsMobile = document.querySelector("#buttons-mobile");
 
+signupBtn.addEventListener("click", function(){
+    introSelector.style = "display: none";
+    buttonsMobile.style = "display: none";
+    console.log("Hello world");
+})
 
 
