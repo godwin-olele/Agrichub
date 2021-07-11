0ca2c27d8f3b80cb85a5b5b657cba20423bfa792
godwin olele <godwinolele10@gmail.com>
2021-07-11 15:36:08 +0100

    adding new feature

--- script/index.js
+++ script/index.js
@@ -1,11 +1,28 @@
 const signupBtn = document.querySelector("#signup-Btn");
 const introSelector = document.querySelector("#intro");
 const buttonsMobile = document.querySelector("#buttons-mobile");
+const signUpZone = document.querySelector("#sign-up-container");
+
 
 signupBtn.addEventListener("click", function(){
     introSelector.style = "display: none";
     buttonsMobile.style = "display: none";
-    console.log("Hello world");
+    document.getElementById("sign-up-container").style.display = "block";
+  
+
+    const styles = {
+        position: 'absolute',
+        bottom: '0',
+        boxSizing: 'border-box',
+        width: '100%',
+        height: '70vh',
+        padding: '50px 30px',
+        backgroundColor: '#fff',
+        borderRadius: '70px 0 70px 0'
+      }
+      
+      Object.assign(signUpZone.style, styles);
+
 })
 
 
