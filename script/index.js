ee66408249b326f654984bc3c1672a49772bef69
Saif Shaikh <59909627+Saif-S@users.noreply.github.com>
2021-07-11 20:09:32 +0530

    Saif s (#51)
    * issue #25: added cabbage.html page
    
    * issue #24: added lettuce.html page
    
    * issue #37 - added garlic.html page
    
    * issue #34 - added spinach.html page
    
    * issue #33 - added broccoli.html page
    
    * issue #45 - aligned html checkbox
    
    * issue #44 - show error message if details not filled on index.html page
    
    Co-authored-by: iRish <godwinolele10@gmail.com>

--- script/index.js
+++ script/index.js
@@ -1,7 +1,17 @@
 const signupBtn = document.querySelector("#signup-Btn");
+const signupWebBtn = document.querySelector("#signUp");
 const introSelector = document.querySelector("#intro");
 const buttonsMobile = document.querySelector("#buttons-mobile");
+
+const nameError = document.querySelector("#nameError");
+const emailError = document.querySelector("#emailError");
+const passwordError = document.querySelector("#passwordError");
+const nameField = document.querySelector("#name");
+const emailField = document.querySelector("#email");
+const passwordField = document.querySelector("#password");
+
 const signUpZone = document.querySelector("#sign-up-container");
+
 
 
 signupBtn.addEventListener("click", function(){
@@ -23,6 +33,35 @@ signupBtn.addEventListener("click", function(){
       
       Object.assign(signUpZone.style, styles);
 
+})
+
+signupWebBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if(nameField.value === "" || null || nameField.length === 0 ){
+       nameError.innerHTML = "Please enter a name!";
+    }
+    if(emailField.value === "" || null || emailField.length === 0 ){
+       emailError.innerHTML = "Please enter a email!";
+    }
+    if(passwordField.value === "" || null || passwordField.length === 0 ){
+       passwordError.innerHTML = "Please enter a password!";
+    }
+})
+
+nameField.addEventListener('input', () => {
+    if(nameField.value !== "" || null || nameField.length !== 0 ){
+        nameError.innerHTML = "";
+    }
+})
+emailField.addEventListener('input', () => {
+    if(emailField.value !== "" || null || emailField.length !== 0 ){
+        emailError.innerHTML = "";
+    }
+})
+passwordField.addEventListener('input', () => {
+    if(passwordField.value !== "" || null || passwordField.length !== 0 ){
+        passwordError.innerHTML = "";
+    }
 })
 
 
