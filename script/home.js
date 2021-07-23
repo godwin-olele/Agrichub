204aa9f0c38d23d5cc40e38ea07af24cdcbeee9b
godwin olele <godwinolele10@gmail.com>
2021-07-23 12:03:55 +0100

    added feature and about section on home page

--- script/home.js
+++ script/home.js
@@ -3,3 +3,4 @@ const selectElement = (element) => document.querySelector(element);
 selectElement('.menu-icons').addEventListener('click', () => {
     selectElement('nav').classList.toggle('active');
 })
+
